9c7221a90bdff1185a100f8ed02988b93c58a5c1
Andre Santos <andresantosbr@gmail.com>
2026-03-07 16:42:13 -0300

    feat (runtime): creating alarms

--- infra/runtime/dev/cloudwatch.tf
+++ infra/runtime/dev/cloudwatch.tf
@@ -7,3 +7,20 @@ resource "aws_cloudwatch_log_group" "this" {
   }
 
 }
+
+resource "aws_cloudwatch_metric_alarm" "5xx_errors" {
+  alarm_name          = "${var.project_name}-5xx-errors-${var.env}"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "HTTPCode_Target_5XX_Count"
+  namespace           = "AWS/ApplicationELB"
+  period              = 60
+  statistic           = "Sum"
+  threshold           = 5
+
+  dimensions = {
+    LoadBalancer = aws_lb.this.arn_suffix
+    TargetGroup  = aws_lb_target_group.this.arn_suffix
+  
+  }
+}
